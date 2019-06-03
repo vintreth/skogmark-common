3b9770c9b80b38fab20d9dfbfa3c44354d821629
svip <bogdanow90@gmail.com>
2019-06-04 00:03:29 +0300

    logs

--- src/main/java/ru/skogmark/framework/request/OutgoingRequestService.java
+++ src/main/java/ru/skogmark/framework/request/OutgoingRequestService.java
@@ -4,6 +4,8 @@ import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
@@ -13,6 +15,8 @@ import java.util.function.Consumer;
 import static java.util.Objects.requireNonNull;
 
 public class OutgoingRequestService {
+    private static final Logger log = LoggerFactory.getLogger(OutgoingRequestService.class);
+
     private final HttpClient httpClient;
     private final Executor outgoingRequestExecutor;
 
@@ -24,6 +28,7 @@ public class OutgoingRequestService {
     public void execute(HttpHost target, HttpRequest request, Consumer<HttpResponse> onResponseReceivedCallback) {
         outgoingRequestExecutor.execute(() -> {
             try {
+                log.info("Sending request: target={}. request={}", target, request);
                 onResponseReceivedCallback.accept(httpClient.execute(target, request));
             } catch (IOException e) {
                 throw new IllegalStateException("Exception caught during http request execution", e);
